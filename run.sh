50e4614aa3473acb610ec3dd5b52832dd01a488b
quynhnn89 <quynhnguyenngoc89@gmail.com>
2022-08-29 20:37:16 +0200

    initial commit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git add --all
 git commit -m "initial commit"
-git push https:/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@github.com/quynhnn89/homepage.git
+git push https://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@github.com/quynhnn89/homepage.git
